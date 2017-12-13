550bc496410e6537f14e651476213d89cade7285
Bruno Alla <browniebroke@users.noreply.github.com>
2017-12-13 13:54:12 +0000

    Add Py 3.5 and 3.6 trove classifiers in setup.py

--- setup.py
+++ setup.py
@@ -176,6 +176,8 @@ setup_kwargs = dict(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 )
 
